fc82c0667b56a755ecd20a7c3b21f90c2b51d9e5
Jithin <jithin.kumar@mileseducation.com>
2022-12-12 12:04:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 4000;
+const programmingLanguagesRouter = require('./src/routes/programmingLanguages.route');
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use('/programming-languages',jsonParser,urlencodedParser, programmingLanguagesRouter);
+
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
+  
+  return;
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
---
+++ src/configs/db.config.js
@@ -0,0 +1,26 @@
+const env = process.env;
+const fs = require('fs');
+const db = {
+    host: env.DB_HOST,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME || 'programming_languages',
+    port: env.DB_PORT || 3306,
+};
+
+module.exports = db;
+
+
+
+// for production to config ssl //
+// const db = {
+//   host: env.DB_HOST,
+//   user: env.DB_USER,
+//   password: env.DB_PASSWORD,
+//   database: env.DB_NAME || 'programming_languages',
+//   port: env.DB_PORT || 3306,
+//   ssl: {
+//     mode: 'VERIFY_IDENTITY',
+//     ca: fs.readFileSync('/etc/ssl/cert.pem', 'utf-8'),
+//   }
+// };
